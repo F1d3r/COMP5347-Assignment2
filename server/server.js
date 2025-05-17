b16a6d7e881107180eab3abccc8033329b77874a
gzha0824 <guangningzh4ng@gmail.com>
2025-05-17 21:36:09 +1000

    Deleted auth router and controller in server

--- server/server.js
+++ server/server.js
@@ -61,13 +61,9 @@ app.use((req, res, next) => {
 
 // Router
 const userRouter = require('./routes/user.routes');
-const authRouter = require('./routes/auth.routes');
 const phoneRouter = require('./routes/phone.routes');
 app.use('/user', userRouter);
 app.use('/phone', phoneRouter);
-app.use('/auth', authRouter);
-
-
 
 // Error handling middleware
 app.use((err, req, res, next) => {
